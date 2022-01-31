74b61944bf25b0628a8ae5d2b19932a17bfd98ad
Rossi <rossi.oddet@dev-institut.fr>
2022-01-31 17:24:43 +0100

    Spring MVC - Démo : Mapping requête HTTP

--- 01-demo/04-demo-spring-mvc/src/main/java/dev/controller/PlatCtrl.java
+++ 01-demo/04-demo-spring-mvc/src/main/java/dev/controller/PlatCtrl.java
@@ -22,7 +22,31 @@ public class PlatCtrl {
 //            path = "hello2"
 //    )
     @GetMapping("hello2")
+    // hello2?nom=?&prenom=?
     public String bonjour2(@RequestParam String prenom, @RequestParam String nom) {
         return "Hello " + prenom + " " + nom;
+    }
+
+    // http://localhost:8080/a1/b1/c1?p1=?&p2=?
+    // PROTOCOL://MACHINE:PORT/PATH?PARAMS
+
+    @GetMapping("hello3/{nom}/{prenom}/details")
+    // hello3/{nom}/{prenom}/details?age=X
+    public String bonjour3(@RequestParam Integer age, @PathVariable String prenom, @PathVariable String nom) {
+        return "Hello " + prenom + " " + nom + " age : " + age;
+    }
+
+    @GetMapping("hello4/{nom}/{prenom}/details")
+    // hello4/{nom}/{prenom}/details?age=X
+    public String bonjour4(
+            @RequestParam Integer age,
+            @PathVariable String prenom,
+            @PathVariable String nom,
+            @RequestHeader("User-Agent") String userAgent,
+            @RequestHeader("mon-entete-a-moi") String monEntete,
+            @RequestBody String texte) {
+        return "Hello " + prenom + " " + nom + " age : " + age
+                + " User Agent : " + userAgent + " mon entête : " + monEntete
+                + " corps : " + texte;
     }
 }
